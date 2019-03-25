5c3dfc3ad9f23ca33df6c7fed46f0b2944426bdd
tony-albanese <tony.albanese2000@gmail.com>
2019-03-25 12:54:34 +0100

    Second commit test.

--- app/src/main/java/com/philipraschle/itgradedapp/First_Screen.java
+++ app/src/main/java/com/philipraschle/itgradedapp/First_Screen.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-
+//Testing comment.
 
 public class First_Screen extends AppCompatActivity {
     private EditText enteryourName;
