bbf68a2b122f04a3f5a78ae56d79bbb1e44a291b
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 09:56:21 +0200

    Added TODOs.

--- app/src/main/java/com/philipraschle/itgradedapp/NewsActivity.java
+++ app/src/main/java/com/philipraschle/itgradedapp/NewsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class NewsActivity extends AppCompatActivity {
 
+    //TODO: This Activity should get the search url from the intent, launch the search, and populate the views from the data.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
