7042bca72b0d5e87e3ddeb81c2cd42535e657e5b
tony-albanese <tony.albanese2000@gmail.com>
2019-04-11 16:22:49 +0200

    Minor change

--- app/src/main/java/com/philipraschle/itgradedapp/NewsActivity.java
+++ app/src/main/java/com/philipraschle/itgradedapp/NewsActivity.java
@@ -7,7 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
+public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     //TODO: This Activity should get the search url from the intent, launch the search, and populate the views from the data.
 
