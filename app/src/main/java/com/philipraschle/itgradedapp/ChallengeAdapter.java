3b6915f22bda16e989d4713a770cec3f137368f4
Kradd0n <philip-raschle@is-hr.de>
2019-05-07 14:40:16 +0200

    We set up the recycler view and the adapter.

--- app/src/main/java/com/philipraschle/itgradedapp/ChallengeAdapter.java
+++ app/src/main/java/com/philipraschle/itgradedapp/ChallengeAdapter.java
@@ -1,3 +1,5 @@
+package com.philipraschle.itgradedapp;
+
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -5,7 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.philipraschle.itgradedapp.ChallengeResponse.ItemsItem;
+import com.philipraschle.itgradedapp.ChallengeResponse.MetadataItem;
 import com.philipraschle.itgradedapp.ChallengeResponse.Response;
+
+import java.util.List;
 
 
 public class ChallengeAdapter extends RecyclerView.Adapter<ChallengeAdapter.ChallengeViewHolder> {
@@ -26,6 +32,17 @@ public class ChallengeAdapter extends RecyclerView.Adapter<ChallengeAdapter.Chal
 
     @Override
     public void onBindViewHolder(@NonNull ChallengeViewHolder challengeViewHolder, int i) {
+        List<ItemsItem> listOfItems = responseObject.getItems();
+
+        ItemsItem challenge = listOfItems.get(i);
+        List<MetadataItem> metadataItemList = challenge.getMetadata();
+
+        for (MetadataItem metadata : metadataItemList){
+            if(metadata.getKey() == "name"){
+                challengeViewHolder.challengeTextView.setText(metadata.getValue());
+            }
+
+        }
 
     }
 
--- app/src/main/java/com/philipraschle/itgradedapp/NewsActivity.java
+++ app/src/main/java/com/philipraschle/itgradedapp/NewsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.philipraschle.itgradedapp.ChallengeResponse.Response;
@@ -52,6 +54,14 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     public void setupRecyclerView(String data){
         Gson gson = new Gson();
         Response responseObject = gson.fromJson(data, Response.class);
+
+        RecyclerView recyclerView = findViewById(R.id.challenge_recyler_view);
+
+        ChallengeAdapter adapter = new ChallengeAdapter(responseObject);
+
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
